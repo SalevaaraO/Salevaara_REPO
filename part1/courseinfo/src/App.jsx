8259ad65b79288940adff3add27998e3db6b93aa
Okko Sälevaara <okko@salevaara.fi>
2026-09-16 14:11:52 +0300

    keskeneräne

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -2,7 +2,7 @@ const Header = (prop) => {
   console.log(prop)
   return (
     <h1>
-      {prop.course}
+      {prop.course.name}
     </h1>
   )
 }
@@ -10,9 +10,9 @@ const Header = (prop) => {
 const Content = (prop) => {
   return (
     <div>
-      <Part part={prop.part1} exercises={prop.exercises1} />
-      <Part part={prop.part2} exercises={prop.exercises2} />
-      <Part part={prop.part3} exercises={prop.exercises3} />
+      {prop.parts.map(part => (
+        <Part>part={part.name} exercises={part.exercises}</Part>
+      ))}
     </div>
   )
 }
@@ -35,19 +35,28 @@ const Total = (prop) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14 
-
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      },
+    ]
+  }
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
