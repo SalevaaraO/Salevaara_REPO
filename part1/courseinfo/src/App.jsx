2a36e869cd306861ac163dca084ab8a03726824b
Okko Sälevaara <okko@salevaara.fi>
2026-09-18 10:27:33 +0300

    Jäi Old React

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -44,15 +44,65 @@ const Display = ({ counter }) => {
   )
 }
 
-const Button = ({ onClick, text }) => {
+/*const Button = ({ onClick, text }) => {
   return (
     <button onClick={onClick}>
       {text}
     </button>
   )
+}*/
+
+const History = (props) => {
+  if (props.allClicks.length == 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
 }
 
+const Button = ({ onClick, text }) => <button onClick ={onClick}>{text}</button>
+
 const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    setTotal(updatedLeft + right)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    const updatedRight = right + 1
+    setRight(updatedRight)
+    setTotal(left + updatedRight)
+  }
+
+  return (
+    <div>
+      {left}
+      <Button onClick={handleLeftClick} text='left'/>
+      <Button onClick={handleRightClick} text='right' />
+      {right}
+      <History allClicks={allClicks} />
+    </div>
+  )
+}
+
+export default App
+
+/*const App = () => {
   const [ counter, setCounter ] = useState(0)
   console.log('rendering with counter value', counter)
 
